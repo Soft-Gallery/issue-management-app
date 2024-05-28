bd2d82dcf96a60cd562d67467717b3d526b80908
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-05-28 23:08:27 +0900

    feat: 어플리케이션 도입

--- App.tsx
+++ App.tsx
@@ -2,15 +2,27 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginPage from './src/pages/LoginPage';
+import theme from "./src/style/theme";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={LoginPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Home"
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: theme.color.background,
+                    },
+                    headerTintColor: theme.color.white,
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                    },
+                }}
+            >
+                <Stack.Screen name="Home" component={LoginPage} options={{ title: '' }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,20 +1,87 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 import React from 'react';
+import theme from "../style/theme";
+import {WINDOW_WIDTH} from "../const/window";
 
 const LoginPage = () => {
   return (
-    <View>
-      <Text>로그인 페이지</Text>
-    </View>
+      <View style={styles.container}>
+        <View style={styles.uiContainer}>
+          <Image source={require('../assets/project_panda_without_background.png')} style={styles.logo} />
+          <Text style={styles.normalText}>Project Panda</Text>
+          <View style={styles.socialButtonContainer}>
+            <TouchableOpacity
+                onPress={() => {}}
+                style={styles.socialButton}
+            >
+              <Text style={styles.socialText}>로그인 하기</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => {}}
+                style={styles.socialButton}
+            >
+              <Text style={styles.socialText}>회원가입 하기</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: theme.color.background,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+    alignSelf: 'center',
+  },
+  uiContainer: {
+    width: WINDOW_WIDTH - 52,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    marginBottom: 120,
+  },
+  normalText: {
+    color: theme.color.gray6,
+    fontSize: 22,
+    fontWeight: '500',
+    marginBottom: 20,
+    lineHeight: 21,
+    letterSpacing: -0.42,
+  },
+  socialButtonContainer: {
+    display: 'flex',
+    width: '100%',
+    marginTop: 14,
+    gap: 16,
+  },
+  socialButton: {
+    width: '100%',
+    borderRadius: 5,
+    height: 48,
+    backgroundColor: theme.color.gray2,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    padding: 13,
+  },
+  gradientBorder: {
+    padding: 1,
+    borderRadius: 7,
+  },
+  socialText: {
+    color: theme.color.white,
+    fontSize: 15,
+    fontStyle: 'normal',
+    fontWeight: '500',
+    lineHeight: 22.5,
+    letterSpacing: -0.45,
+    marginLeft: 12,
   },
 });
 
