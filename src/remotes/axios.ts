327beb3c4c1afdee111f9c8b60bb63420022075b
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-06-03 11:44:07 +0900

    fix: 서버 주소 직접 입력

--- src/remotes/axios.ts
+++ src/remotes/axios.ts
@@ -2,7 +2,7 @@ import axios, { CreateAxiosDefaults } from 'axios';
 import { REACT_APP_SERVER_URL } from '@env';
 
 const defaultConfig: CreateAxiosDefaults = {
-    baseURL: REACT_APP_SERVER_URL,
+    baseURL: 'https://projectpanda.ngrok.io',
 };
 
 const client = axios.create(defaultConfig);
